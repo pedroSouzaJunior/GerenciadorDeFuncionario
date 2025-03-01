98510d78875f22652baf2c3193f82acb7d3a4e91
Pedro Souza Junior <pedro.junior@ms.senac.br>
2025-03-01 14:53:52 -0400

    atualizando migrations para adicionar Role de admin e criacao de user padrao

---
+++ GerenciadorFuncionarios.Infraestrutura/Migrations/20250301183824_UpdateAdminRole.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorFuncionarios.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAdminRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE Funcionarios 
+                SET Role = 'Admin' 
+                WHERE Email = 'admin@email.com'
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE Funcionarios 
+                SET Role = NULL 
+                WHERE Email = 'admin@email.com'
+            ");
+        }
+    }
+}
---
+++ GerenciadorFuncionarios.Infraestrutura/Migrations/20250301185021_SeedNormalUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorFuncionarios.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedNormalUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string senhaUser = BCrypt.Net.BCrypt.HashPassword("user123");
+
+            migrationBuilder.Sql($@"
+            INSERT INTO Funcionarios (Nome, Sobrenome, Email, Documento, DataNascimento, GestorId, SenhaHash, Role) 
+            VALUES ('Usuario', 'Comum', 'user@email.com', '40792326503', '1995-05-15', NULL, '{senhaUser}', 'Usuario')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Funcionarios WHERE Email IN ('user@email.com')");
+        }
+    }
+}
